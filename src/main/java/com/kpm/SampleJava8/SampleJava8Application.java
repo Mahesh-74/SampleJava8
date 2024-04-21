a93d1a730831ea02d92135c768e5c463d1ceef6b
Mahesh-74 <mahesh74.java@gmail.com>
2024-04-21 20:52:01 +0530

    FM

---
+++ src/main/java/com/kpm/SampleJava8/SampleJava8Application.java
@@ -0,0 +1,13 @@
+package com.kpm.SampleJava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJava8Application.class, args);
+	}
+
+}
