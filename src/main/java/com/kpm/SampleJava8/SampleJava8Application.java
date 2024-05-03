705b3f5f5c78d1bcc74d4646c0b21ec4d84c803d
Mahesh-74 <mahesh74.java@gmail.com>
2024-05-03 18:12:27 +0530

    Updated code.

--- src/main/java/com/kpm/SampleJava8/SampleJava8Application.java
+++ src/main/java/com/kpm/SampleJava8/SampleJava8Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleJava8Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(SampleJava8Application.class, args);
 	}
 
